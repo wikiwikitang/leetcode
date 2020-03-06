154b544c4d1a2c7644d7a9839d8a95ffaa33637d
wikiwikitang <ctang13@binghamton.edu>
2020-03-06 00:15:52 -0800

    0226

---
+++ 0226-binarySearch-invertBinaryTree.js
@@ -0,0 +1,69 @@
+/*
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (!root || (!root.left && !root.right)) {
+    return root;
+  }
+
+  let left = invertTree(root.left);
+  let right = invertTree(root.right);
+  root.right = left;
+  root.left = right;
+  return root;
+};
+
+//take the advantage of es6
+var invertTree2 = function(root) {
+  if (!root || (!root.left && !root.right)) {
+    return root;
+  }
+  [root.right, root.left] = [invertTree(root.left), invertTree(root.right)];
+
+  return root;
+};
+
+//dfs iterative
+var invertTree2 = function(root) {
+  if (!root || (!root.left && !root.right)) {
+    return root;
+  }
+  let stack = [root];
+  while (queue.length > 0) {
+    let node = queue.pop();
+    if (node) {
+      [node.left, node.right] = [node.right, node.left];
+      queue.push(node.left, node.right);
+    }
+  }
+  return root;
+};
